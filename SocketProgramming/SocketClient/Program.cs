7d8439a93f82cd890c77c73e163b287a06da67a9
Ermand Haruni <88191097+ErmandH@users.noreply.github.com>
2024-07-26 14:01:13 +0300

    socket

---
+++ SocketProgramming/SocketClient/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Program
+    {
+        public class SyncSocketClient
+        {
+            public static void StartClient()
+            {
+                // gelicek olan mesaj için oluşturulan buffer
+                byte[] buffer = new byte[1024];
+                try
+                {
+                    var hostName = Dns.GetHostName();
+                    IPHostEntry ipHost = Dns.GetHostEntry(hostName);
+                    IPAddress ip = ipHost.AddressList[0];
+                    IPEndPoint remoteEp = new IPEndPoint(ip, 43665);
+
+                    // bağlanılacak olan soketi oluşturma
+                    Socket sender = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                    try
+                    {
+                        // sokete bağlanma
+                        sender.Connect(remoteEp);
+                        Console.WriteLine("Socket connected");
+                        while (true)
+                        {
+                            Console.Write("Client: ");
+                            string msgStr = Console.ReadLine();
+                            byte[] msg = Encoding.ASCII.GetBytes(msgStr);
+
+                            // gönderme işlemi
+                            int bytesSent = sender.Send(msg);
+
+                            if (msgStr.IndexOf("<EOF>") > -1)
+                            {
+                                Console.WriteLine("Ending client");
+                                return;
+                            }
+
+                            int bytesReceived = sender.Receive(buffer);
+                            string msgReceived = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+                            Console.WriteLine("Server: " + msgReceived);
+                        }                 
+
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    catch (Exception e){
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("press any key to connect");
+            Console.ReadLine();
+
+            SyncSocketClient.StartClient();
+            Console.Write("press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SocketProgramming/SocketServer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    class Program
+    {
+        public class SyncServerSocket
+        {
+            public static string data = null;
+
+            public static void StartListener()
+            {   
+                byte[] buffer = new byte[1024];
+                int maxListenerCount = 10;
+                var hostName = Dns.GetHostName();
+                IPHostEntry ipHost = Dns.GetHostEntry(hostName);
+                IPAddress ip = ipHost.AddressList[0];
+                // serverın çalışacağı portun ayarlanması
+                IPEndPoint localEp = new IPEndPoint(ip, 43665);
+
+                Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    // bağlantı beklenecek olan endpointi sokete bağla
+                    listener.Bind(localEp);
+
+                    // serverın dinleme işlemini başlat
+                    listener.Listen(maxListenerCount);
+                    while (true)
+                    {
+                        Console.WriteLine("Waiting for a connection.....");
+                        // bağlantı isteği geldiğinde kabul et
+                        Socket handler = listener.Accept();
+                        Console.WriteLine("------ New Socket Connected ------");
+                        data = null;
+                        // end of file görene kadar clientı dinlemeye devam et
+                        while (true)
+                        {
+                            int byteRec = handler.Receive(buffer);
+                            data = Encoding.ASCII.GetString(buffer, 0, byteRec);
+                            Console.WriteLine("Client: " + data);
+                            // gelen mesajda EOF varsa döngüyü bitir
+                            if (data.IndexOf("<EOF>") > -1)
+                            {
+                                handler.Shutdown(SocketShutdown.Both);
+                                handler.Close();
+                                break;
+                            }
+                            Console.Write("Server: ");
+                            string msgStr = Console.ReadLine();
+                            byte[] msg = Encoding.ASCII.GetBytes(msgStr);
+                            handler.Send(msg);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine("press any to exit program");
+                Console.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SyncServerSocket.StartListener();
+        }
+    }
+}
